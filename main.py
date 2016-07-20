43a8efcf0ef9b343cdca123cfb201f70dc85c3e1
sven-oly <cwcornelius@gmail.com>
2016-07-19 22:10:53 -0700

    Completely redo chrCalculator.

---
+++ main.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import json
+import logging
+import os
+import webapp2
+
+from google.appengine.ext.webapp import template
+
+class MainHandler(webapp2.RequestHandler):
+  def get(self):
+    #        self.response.write('ᎣᏏᏲ world!')
+    
+    template_values = {
+      'name': 'ᎣᏏᏲ',
+      'lang': 'en',
+    }
+    path = os.path.join(os.path.dirname(__file__), 'chrNumerals.html')
+    self.response.out.write(template.render(path, template_values))
+
+class SimpleHandler(webapp2.RequestHandler):
+  def get(self):
+    #        self.response.write('ᎣᏏᏲ world!')
+    
+    template_values = {
+      'name': 'ᎣᏏᏲ',
+      'lang': 'en',
+    }
+    path = os.path.join(os.path.dirname(__file__), 'calculator.html')
+    self.response.out.write(template.render(path, template_values))
+    
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/simple/', SimpleHandler)
+], debug=True)
